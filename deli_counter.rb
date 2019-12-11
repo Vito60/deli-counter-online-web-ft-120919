e2b8173fbebacd6262d1dd3294425de8d827dd93
Randall Williams <randallw1996@gmail.com>
2019-12-11 19:58:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,10 @@ def line(array)
   end
 end
 
+def line_simple(array) 
+  current_line = "The simple line is currently:"
+  array.each.with_index(1) do |value, indexemus|  
+    current_line << " #{indexemus}. #{value},"    
+  end 
+  puts current_line
+end 
