d6f373dd22847a995ecb121a313eaceeab9acb30
Randall Williams <randallw1996@gmail.com>
2019-12-11 19:40:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+require 'pry'
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  elsif katz_deli.length > 0 
+    katz_deli.each do |name|
+      
+    
+  end
+end
+
+  
