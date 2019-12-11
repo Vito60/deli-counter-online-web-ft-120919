3ddb7ace5d830d491b55f455660771d17636ae16
Randall Williams <randallw1996@gmail.com>
2019-12-11 19:55:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,2 @@
-def line(katz_deli)
-if katz_deli.length > 0 
-  new_array = []
-  counter = 1 
-  katz_deli.each do |name|
-    new_array.push("#{counter}. #{name}")
-    counter += 1 
-  end
-  puts "The line is currently: #{new_array.join(" ")"
-else 
-  puts "The line is currently empty."
-end
 
   
