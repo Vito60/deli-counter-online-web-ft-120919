7d633b0ea427956e39f4512a55d701d627d4270d
Randall Williams <randallw1996@gmail.com>
2019-12-11 19:45:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif katz_deli.length > 0
-    string_text = "The line is currently: "
-    
+    new_array = []
+    counter = 1
     katz_deli.each do |name|
       
     
