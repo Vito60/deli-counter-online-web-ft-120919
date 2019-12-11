204f5894761fe62a338c6e4981951f1a5d562597
Randall Williams <randallw1996@gmail.com>
2019-12-11 19:57:26 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,13 @@
+def line(array)
+  if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
