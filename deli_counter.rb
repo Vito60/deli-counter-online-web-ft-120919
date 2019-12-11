2dddb4e249327d34896380a060f02c04f5389f75
Randall Williams <randallw1996@gmail.com>
2019-12-11 20:01:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,11 +17,5 @@ def take_a_number(line, new_person)
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
 
-def now_serving(line)
-  if line.length == 0 
-    puts"There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{line[0]}." # could say: 
-  end
-end
+
 
